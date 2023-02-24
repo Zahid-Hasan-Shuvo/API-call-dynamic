6ef3df85f6f062edb7608221ab20d039bbfd3558
Zahid-Hasan-Shuvo <shuvo.cse6.bu@gmail.com>
2023-02-24 20:02:58 +0600

    show flac dianamicly

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ countries.forEach(country => {
   countryDiv.innerHTML=`
   <h3> Name:${country.name.common} </h3>
   <p> Capital: ${country.capital? country.capital[0] : "No Capital"}</p>
+
+  <button onclick="loadCountryDetails('${country.cca2}')" > Details </button>
   
   
   `;
@@ -22,6 +24,26 @@ countries.forEach(country => {
   countriesContainer.appendChild(countryDiv)
 })
 
+}
+
+
+const loadCountryDetails =code =>{
+ const url =`https://restcountries.com/v3.1/alpha/${code}`
+
+ fetch(url)
+ .then(res=> res.json())
+ .then( data => showCountryDetail(data[0]))
+
+
+}
+
+const showCountryDetail= country =>{
+  const detailContainer=document.getElementById('country-id')
+
+  detailContainer.innerHTML=` 
+  <h3> Name: ${country.name.common} </h3>
+  <img src="${country.flags.png}">
+  `
 }
 
 
